b2403eaf6e98c4c7becdfad633bbb5e7ece50aa4
mayanjer <mayanjer3@gmail.com>
2025-06-01 11:14:30 +0300

    created a table

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 
 from .views.general_views import index_view
-from .views.product_views import manage_product_view
+from .views.product_views import manage_product_view, add_product_view 
 from .views.accounts_views import manage_accounts_view
 
 urlpatterns = [
     path('index.html', index_view, name='index_page'),
     path('products.html', manage_product_view, name='products_page'),
     path('accounts.html', manage_accounts_view, name='accounts_page'),
+    path('add_product.html', add_product_view, name = 'add_products_page')
 
 ]
--- app/views/product_views.py
+++ app/views/product_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from app.forms.product_forms import ProductForm
 from app.selectors.product_selectors import get_all_products
+from app.models.products import Product
 
 def manage_product_view(request):
     product_form = ProductForm()
@@ -13,6 +14,29 @@ def manage_product_view(request):
         'products': products
     }
     return render(request, 'products.html', context)
+
+def add_product_view(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('name')
+        sku = request.POST.get('sku')
+        description = request.POST.get('name')
+        barcode = request.POST.get('barcode')
+        uuid_code = request.POST.get('uuid_code')
+        category = request.POST.get('category')
+        is_active = request.POST.get('is_active')
+        created_at = request.POST.get('name')
+
+        product = Product.objects.create(
+            name = product_name,
+            sku = sku,
+            description = description,
+            barcode = barcode,
+            category = category,
+            created_at = created_at
+            )
+        
+        product.save()
+    return render(request, 'add_product.html')
 
 def update_products_view(request):
     pass
