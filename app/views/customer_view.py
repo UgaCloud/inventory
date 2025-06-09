157ecf6678d6457ccf36e33a910ff075ae02b37c
mayanjer <mayanjer3@gmail.com>
2025-06-09 14:08:42 +0300

    added some pages

--- app/views/customer_view.py
+++ app/views/customer_view.py
@@ -3,18 +3,4 @@ from app.forms.customer_forms import CustomerForm
 from app.selectors.customer_selectors import get_all_customers
 
 def customer_view(request):
-    if request.method == "POST":
-        form = CustomerForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-    else:
-        form = CustomerForm()
-    
-    customers = get_all_customers()
-
-    context = {
-        'form':form,
-        'customers':customers
-    }
-    return render(request, 'customers.html', context)
+    return render(request, 'customer_list.html')
