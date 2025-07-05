70ca60d6834d8a831f1ed9444af78876cf53f921
Abdulhakim <walusimbi96@gmail.com>
2025-07-05 21:22:18 +0300

    Added database setting for production

--- core/settings/production.py
+++ core/settings/production.py
@@ -9,3 +9,14 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 ALLOWED_HOSTS = ['kafunda-ug.com', 'www.kafunda-ug.com', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  
+        'PORT': '3306',
+    }
+}
